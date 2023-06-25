6e5a6a39e0fcf54ed93fcbbbee7e7265ef797460
uzelkerem <uzelkerem@gmail.com>
2023-06-25 19:54:26 +0200

    python script fix

--- test_glob_wildcards.py
+++ test_glob_wildcards.py
@@ -4,7 +4,7 @@ from snakemake import glob_wildcards
 
 input_folder = "/mnt/groupMansuy/kerem/tasks/longrna/rawdatas"
 
-wildcards = glob_wildcards(f"{input_folder}/{{sample}}.fq.gz")
+wildcards = glob_wildcards(f"{input_folder}/{{sample}}.fastq.gz")
 print(wildcards)
 
 sample_values = wildcards.sample
