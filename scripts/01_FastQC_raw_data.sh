490fee48502fed60f6b7bd87f2e130d5711fe260
uzelkerem <uzelkerem@gmail.com>
2023-08-09 08:40:49 +0200

    deleting unnecessary scripts

--- scripts/01_FastQC_raw_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-input_file=$1
-output_folder=$2
-
-mkdir -p "${output_folder}"
-mkdir -p "${output_folder}/logs"
-
-fastqc -t 4 -o "${output_folder}" "${input_file}" > "${output_folder}/logs/$(basename "${input_file}").log" 2>&1
